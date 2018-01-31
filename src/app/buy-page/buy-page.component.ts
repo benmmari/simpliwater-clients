2b58fa99caffb5d82f554ea37b297757322e2867
Benjamin Mmari <benmmari92@gmail.com>
2018-01-31 19:00:28 +0200

    changes to routing

--- src/app/buy-page/buy-page.component.ts
+++ src/app/buy-page/buy-page.component.ts
@@ -64,8 +64,6 @@ export class BuyPageComponent implements OnInit {
 
 
   topUpWithEth() {
-
-    let tokens = this.calculationService.calculateTokens(this.amountRands);
     console.log(this.simpliwaterService.getAdminAccount());
     let meta;
     this.simpliwaterService.getSimpliWaterContract()
@@ -77,7 +75,7 @@ export class BuyPageComponent implements OnInit {
       .then(() => {
         //for now if there are no errors go to the success page
         console.log("sent transaction");
-        this.router.navigate(["sent/", this.meterAddress], {queryParams:{rands: this.amountRands, eth:this.calculatedEth, tokens:tokens}});
+        this.router.navigate(["sent/", this.meterAddress]);
       })
       .catch(e => {
         console.log(e);
